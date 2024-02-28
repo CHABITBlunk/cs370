a844a923917d6cc380d55798f559e10e1a29f73e
xfbm <cblunk097@gmail.com>
2024-02-28 09:29:01 -0700

    fix segfaulting issue due to accessing wrong part of argv

--- homework/hw3/checker.c
+++ homework/hw3/checker.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   printf("Checker process [%d]: Starting.\n", getpid());
 
-  int fd = atoi(argv[2]);
-  int divisor = atoi(argv[0]);
-  int dividend = atoi(argv[1]);
+  int fd = atoi(argv[1]);
+  int divisor = atoi(argv[2]);
+  int dividend = atoi(argv[3]);
 
-  read(fd, &divisor, sizeof(divisor));
-  read(fd, &dividend, sizeof(dividend));
+  int shmid;
+  read(fd, &shmid, sizeof(shmid));
+  printf("Checker process [%d]: read %lu bytes containing shm ID %d\n", getpid(), sizeof(shmid), shmid);
+  int *shmpt = (int*) shmat(shmid, NULL, 0);
 
   if (!(dividend % divisor)) {
     printf("Checker process [%d]: %d IS divisible by %d.\n", getpid(), dividend, divisor);
-    printf("Checker process [%d]: Returning 1.\n", getpid());
-    return 1;
+    *shmpt = 1;
+    printf("Checker process [%d]: wrote result (%d) to shared memory.\n", getpid(), *shmpt);
   } else {
     printf("Checker process [%d]: %d *IS NOT* divisible by %d.\n", getpid(), dividend, divisor);
-    printf("Checker process [%d]: Returning 0.\n", getpid());
-    return 0;
+    *shmpt = 0;
+    printf("Checker process [%d]: wrote result (%d) to shared memory.\n", getpid(), *shmpt);
   }
+  shmdt(shmpt);
+  return 0;
 }
--- homework/hw3/coordinator.c
+++ homework/hw3/coordinator.c
@@ -8,36 +8,53 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+  if (argc < 5) {
+    printf("not enough arguments (required: 5).");
+    return -1;
+  }
+  int shmids[4];
   for (int i = 2; i < argc; i++) {
-    
-    int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+
+    shmids[i - 2] = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
 
     int fd[2];
     pipe(fd);
 
     pid_t pid = fork();
 
-    if (pid == -1) {
+    if (pid < 0) {
       printf("Fork failed.\n");
     }
-    else if (pid == 0) {
+
+    else if (pid > 0) {
       printf("Coordinator: forked process with id %d.\n", getpid());
 			close(fd[0]);
-			write(fd[1], &shmid, sizeof(shmid));
-			printf("Coordinator: wrote shm ID %d to pipe (%d bytes)", shmid, sizeof(shmid));
-      execl("checker", NULL, fd, argv[1], argv[i]);
+			write(fd[1], &shmids[i - 2], sizeof(shmids[i - 2]));
+			printf("Coordinator: wrote shm ID %d to pipe (%lu bytes)\n", shmids[i - 2], sizeof(shmids[i - 2]));
     }
-    else {
-      // wait for child to finish
-      int status;
-      wait(&status);
-      
-      // attach to & read from shm segment
-      int *shmpt = (int*) shmat(shmid, NULL, 0);
 
-      // destroy shmpt
-      shmctl(*shmpt, IPC_RMID, NULL);
+    else {
+      char buffer[8];
+      sprintf(buffer, "%d", fd[0]);
+      execl("checker", "checker", buffer, argv[1], argv[i]);
     }
+  }
+
+  for (int i = 2; i < argc; i++) {
+    // wait for child to finish
+    int status;
+    wait(&status);
+
+    // attach to & read from shm segment
+    int *shmpt = (int*) shmat(shmids[i - 2], NULL, 0);
+    if (*shmpt == 1) {
+      printf("Coordinator: result %d read from shared memory: %s is divisible by %s.\n", *shmpt, argv[i], argv[1]);
+    } else if (*shmpt == 0) {
+      printf("Coordinator: result %d read from shared memory: %s is not divisible by %s.\n", *shmpt, argv[i], argv[1]);
+    }
+
+    // destroy shmpt
+    shmctl(*shmpt, IPC_RMID, NULL);
   }
 
   printf("Coordinator: exiting.\n");
