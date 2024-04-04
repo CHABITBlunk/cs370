99d761405a5a5b898b1aa89fb57b3a25c72bc96a
chabitblunk <cblunk097@gmail.com>
2024-04-03 22:10:10 -0600

    working fcfs & sjfp

--- homework/hw5/Scheduler.cc
+++ homework/hw5/Scheduler.cc
@@ -1,106 +1,136 @@
-#include <cstdlib>
+#include <algorithm>
+#include <cstddef>
+using namespace std;
+#include <cstdio>
 #include <fstream>
-#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
-class CSVRow {
+class proc {
 public:
-  std::string_view operator[](std::size_t index) const {
-    return std::string_view(&m_line[m_data[index] + 1],
-                            m_data[index + 1] - (m_data[index] + 1));
+  int pid, arrival_time, burst_time, priority, remaining_time;
+  proc(int pid, int arrival_time, int burst_time, int priority) {
+    this->pid = pid;
+    this->arrival_time = arrival_time;
+    this->burst_time = burst_time;
+    this->priority = priority;
+    this->remaining_time = burst_time;
   }
-  std::size_t size() const { return m_data.size() - 1; }
-  void readNextRow(std::istream &str) {
-    std::getline(str, m_line);
-
-    m_data.clear();
-    m_data.emplace_back(-1);
-    std::string::size_type pos = 0;
-    while ((pos = m_line.find(',', pos)) != std::string::npos) {
-      m_data.emplace_back(pos);
-      ++pos;
-    }
-    // check for trailing commas
-    pos = m_line.size();
-    m_data.emplace_back(pos);
-  }
-
-private:
-  std::string m_line;
-  std::vector<int> m_data;
 };
 
-std::istream &operator>>(std::istream &str, CSVRow &data) {
-  data.readNextRow(str);
-  return str;
+vector<proc> read_from_csv(char *filename) {
+  vector<proc> procs;
+  fstream file(filename);
+  string line;
+  while (getline(file, line)) {
+    stringstream sstream(line);
+    string token;
+    vector<int> values;
+    while (getline(sstream, token, ',')) {
+      values.push_back(stoi(token));
+    }
+    int pid = values[0];
+    int arrival_time = values[1];
+    int burst_time = values[2];
+    int priority = values[3];
+
+    proc p(pid, arrival_time, burst_time, priority);
+    procs.emplace_back(p);
+  }
+  return procs;
 }
 
-class CSVIterator {
-public:
-  typedef std::input_iterator_tag iterator_category;
-  typedef CSVRow value_type;
-  typedef std::size_t difference_type;
-  typedef CSVRow *pointer;
-  typedef CSVRow &reference;
-
-  CSVIterator(std::istream &str) : m_str(str.good() ? &str : nullptr) {
-    ++(*this);
+void fcfs(vector<proc> &procs) {
+  printf("--- FCFS ---\n");
+  vector<proc> procs_copy = procs;
+  int total_time = 0, total_turnaround = 0, total_wait = 0,
+      nprocs = procs_copy.size();
+  sort(procs_copy.begin(), procs_copy.end(), [](const proc &a, const proc &b) {
+    if (a.arrival_time == b.arrival_time)
+      return a.pid < b.pid;
+    return a.arrival_time < b.arrival_time;
+  });
+  for (size_t i = 0; i < nprocs; ++i) {
+    proc &proc = procs_copy[i];
+    int waiting_time = total_time - proc.arrival_time;
+    int completion_time = total_time + proc.burst_time;
+    int turnaround_time = completion_time - proc.arrival_time;
+    total_time += proc.burst_time;
+    total_wait += waiting_time;
+    total_turnaround += turnaround_time;
   }
-  CSVIterator() : m_str(nullptr) {}
+  printf("Average Turnaround Time: %.3f\n", total_turnaround / (float)nprocs);
+  printf("Average Wait Time: %.3f\n", total_wait / (float)nprocs);
+  printf("Throughput: %.3f\n", nprocs / (float)total_time);
+}
 
-  // pre-increment
-  CSVIterator &operator++() {
-    if (m_str) {
-      if (!((*m_str) >> m_row)) {
-        m_str = nullptr;
+void sjfp(vector<proc> &procs) {
+  printf("--- SJFP ---\n");
+  int total_time = 0, total_turnaround = 0, total_wait = 0, ncompleted = 0,
+      nprocs = procs.size();
+  vector<bool> completed(procs.size(), false);
+  while (ncompleted != procs.size()) {
+    int ip = -1;
+    int burst = 1000;
+    for (int i = 0; i < procs.size(); i++) {
+      if (procs[i].arrival_time <= total_time && !completed[i]) {
+        if (procs[i].remaining_time < burst) {
+          burst = procs[i].remaining_time;
+          ip = i;
+        }
+        if (procs[i].remaining_time == burst) {
+          ip = i;
+        }
       }
     }
-    return *this;
+    total_time++;
+    if (ip != -1) {
+      procs[ip].remaining_time--;
+      if (procs[ip].remaining_time == 0) {
+        ncompleted++;
+        completed[ip] = true;
+        int completion_time = total_time;
+        int turnaround_time = completion_time - procs[ip].arrival_time;
+        int waiting_time = turnaround_time - procs[ip].burst_time;
+        total_wait += waiting_time;
+        total_turnaround += turnaround_time;
+      }
+    }
   }
-  // post-increment
-  CSVIterator operator++(int) {
-    CSVIterator tmp(*this);
-    ++(*this);
-    return tmp;
-  }
-  CSVRow const &operator*() const { return m_row; }
-  CSVRow const *operator->() const { return &m_row; }
+  printf("Average Turnaround Time: %.3f\n", total_turnaround / (float)nprocs);
+  printf("Average Wait Time: %.3f\n", total_wait / (float)nprocs);
+  printf("Throughput: %.3f\n", nprocs / (float)total_time);
+}
 
-  bool operator==(CSVIterator const &rhs) {
-    return ((this == &rhs) || ((this->m_str == nullptr)));
+void priority(vector<proc> &procs) {
+  printf("--- Priority ---\n");
+  sort(procs.begin(), procs.end(), [](proc &a, proc &b) {
+    if (a.priority == b.priority)
+      return a.pid < b.pid;
+    return a.priority < b.priority;
+  });
+  int total_time = 0, total_turnaround = 0, total_wait = 0,
+      num_procs = procs.size();
+  for (size_t i = 0; i < num_procs; ++i) {
+    proc &proc = procs[i];
+    int waiting_time = total_time - proc.arrival_time;
+    int completion_time = total_time + proc.burst_time;
+    int turnaround_time = completion_time - proc.arrival_time;
+    total_time += proc.burst_time;
+    total_wait += waiting_time;
+    total_turnaround += turnaround_time;
   }
-  bool operator!=(CSVIterator const &rhs) { return !((*this) == rhs); }
-
-private:
-  std::istream *m_str;
-  CSVRow m_row;
-};
+  printf("Average Turnaround Time: %.3f\n",
+         total_turnaround / (float)num_procs);
+  printf("Average Wait Time: %.3f\n", total_wait / (float)num_procs);
+  printf("Throughput: %.3f\n", num_procs / (float)total_time);
+}
 
 int main(int argc, char **argv) {
-  std::ifstream file(argv[1]);
-  // NOTE: format for value in line looks like this: (*loop)[i]
-  // i = 0: pid
-  // i = 1: arrival time
-  // i = 2: burst duration
-  // i = 3: priority
-
-  // fcfs (non-preemptive)
-  std::cout << "--- FCFS ---";
-  int total_time = 0, total_turnaround = 0, total_wait = 0;
-  for (CSVIterator loop(file); loop != CSVIterator(); ++loop) {
-    // get arrival time, which when parsed, shows up in position 1
-    int arrivalTime = atoi((*loop)[1]);
-    // add proc time burst to total time
-    total_time += atoi((*loop)[2]);
-  }
-  // sjfp
-  std::cout << "--- SJFP ---";
-  for (CSVIterator loop(file); loop != CSVIterator(); ++loop) {
-  }
-  // priority (preemptive)
-  std::cout << "--- Priority ---";
-  for (CSVIterator loop(file); loop != CSVIterator(); ++loop) {
-  }
+  vector<proc> procs = read_from_csv(argv[1]);
+  fcfs(procs);
+  sjfp(procs);
+  priority(procs);
   return 0;
 }
