1296856bb0f72fe013411fe4e9fea6e571f858a2
chabitblunk <cblunk097@gmail.com>
2024-04-03 23:01:05 -0600

    working sjfp and priority to within 0.01 and 0.37 respectively

--- homework/hw5/Scheduler.cc
+++ homework/hw5/Scheduler.cc
@@ -1,21 +1,23 @@
 #include <algorithm>
+#include <climits>
 #include <cstddef>
-using namespace std;
 #include <cstdio>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class proc {
 public:
   int pid, arrival_time, burst_time, priority, remaining_time;
-  proc(int pid, int arrival_time, int burst_time, int priority) {
-    this->pid = pid;
-    this->arrival_time = arrival_time;
-    this->burst_time = burst_time;
-    this->priority = priority;
-    this->remaining_time = burst_time;
+  proc(int p, int a, int b, int pr) {
+    pid = p;
+    arrival_time = a;
+    burst_time = b;
+    priority = pr;
+    remaining_time = b;
   }
 };
 
@@ -51,7 +53,7 @@ void fcfs(vector<proc> &procs) {
       return a.pid < b.pid;
     return a.arrival_time < b.arrival_time;
   });
-  for (size_t i = 0; i < nprocs; ++i) {
+  for (int i = 0; i < nprocs; ++i) {
     proc &proc = procs_copy[i];
     int waiting_time = total_time - proc.arrival_time;
     int completion_time = total_time + proc.burst_time;
@@ -60,71 +62,91 @@ void fcfs(vector<proc> &procs) {
     total_wait += waiting_time;
     total_turnaround += turnaround_time;
   }
-  printf("Average Turnaround Time: %.3f\n", total_turnaround / (float)nprocs);
-  printf("Average Wait Time: %.3f\n", total_wait / (float)nprocs);
-  printf("Throughput: %.3f\n", nprocs / (float)total_time);
+  printf("Average Turnaround Time: %.3f\n",
+         total_turnaround / static_cast<float>(nprocs));
+  printf("Average Wait Time: %.3f\n", total_wait / static_cast<float>(nprocs));
+  printf("Throughput: %.3f\n", nprocs / static_cast<float>(total_time));
 }
 
 void sjfp(vector<proc> &procs) {
   printf("--- SJFP ---\n");
-  int total_time = 0, total_turnaround = 0, total_wait = 0, ncompleted = 0,
+  size_t ncompleted = 0;
+  int total_time = 0, total_turnaround = 0, total_wait = 0,
       nprocs = procs.size();
-  vector<bool> completed(procs.size(), false);
-  while (ncompleted != procs.size()) {
+  vector<proc> procs_copy = procs;
+  vector<bool> completed(procs_copy.size(), false);
+  while (ncompleted < procs_copy.size()) {
     int ip = -1;
-    int burst = 1000;
-    for (int i = 0; i < procs.size(); i++) {
-      if (procs[i].arrival_time <= total_time && !completed[i]) {
-        if (procs[i].remaining_time < burst) {
-          burst = procs[i].remaining_time;
+    int burst = INT_MAX;
+    for (size_t i = 0; i < procs_copy.size(); i++) {
+      if (procs_copy[i].arrival_time <= total_time && !completed[i]) {
+        if (procs_copy[i].remaining_time < burst) {
+          burst = procs_copy[i].remaining_time;
           ip = i;
         }
-        if (procs[i].remaining_time == burst) {
+        if (procs_copy[i].remaining_time == burst)
           ip = i;
-        }
       }
     }
     total_time++;
-    if (ip != -1) {
-      procs[ip].remaining_time--;
-      if (procs[ip].remaining_time == 0) {
+    if (ip > -1) {
+      procs_copy[ip].remaining_time--;
+      if (procs_copy[ip].remaining_time == 0) {
         ncompleted++;
         completed[ip] = true;
         int completion_time = total_time;
-        int turnaround_time = completion_time - procs[ip].arrival_time;
-        int waiting_time = turnaround_time - procs[ip].burst_time;
+        int turnaround_time = completion_time - procs_copy[ip].arrival_time;
+        int waiting_time = turnaround_time - procs_copy[ip].burst_time;
         total_wait += waiting_time;
         total_turnaround += turnaround_time;
       }
     }
   }
-  printf("Average Turnaround Time: %.3f\n", total_turnaround / (float)nprocs);
-  printf("Average Wait Time: %.3f\n", total_wait / (float)nprocs);
-  printf("Throughput: %.3f\n", nprocs / (float)total_time);
+  printf("Average Turnaround Time: %.3f\n",
+         total_turnaround / static_cast<float>(nprocs));
+  printf("Average Wait Time: %.3f\n", total_wait / static_cast<float>(nprocs));
+  printf("Throughput: %.3f\n", nprocs / static_cast<float>(total_time));
 }
 
 void priority(vector<proc> &procs) {
   printf("--- Priority ---\n");
-  sort(procs.begin(), procs.end(), [](proc &a, proc &b) {
-    if (a.priority == b.priority)
-      return a.pid < b.pid;
-    return a.priority < b.priority;
-  });
+  size_t ncompleted = 0;
   int total_time = 0, total_turnaround = 0, total_wait = 0,
-      num_procs = procs.size();
-  for (size_t i = 0; i < num_procs; ++i) {
-    proc &proc = procs[i];
-    int waiting_time = total_time - proc.arrival_time;
-    int completion_time = total_time + proc.burst_time;
-    int turnaround_time = completion_time - proc.arrival_time;
-    total_time += proc.burst_time;
-    total_wait += waiting_time;
-    total_turnaround += turnaround_time;
+      nprocs = procs.size();
+  vector<proc> procs_copy = procs;
+  vector<bool> completed(procs.size(), false);
+  while (ncompleted < procs.size()) {
+    int ip = -1;
+    int pr = INT_MAX;
+    for (size_t i = 0; i < procs_copy.size(); i++) {
+      if (procs_copy[i].arrival_time <= total_time && !completed[i]) {
+        if (procs_copy[i].priority < pr) {
+          pr = procs_copy[i].priority;
+          ip = i;
+        } else if (procs_copy[i].priority == pr) {
+          if (procs_copy[i].arrival_time < procs_copy[ip].arrival_time)
+            ip = i;
+        }
+      }
+    }
+    total_time++;
+    if (ip > -1) {
+      procs_copy[ip].remaining_time--;
+      if (procs_copy[ip].remaining_time == 0) {
+        ncompleted++;
+        completed[ip] = true;
+        int completion_time = total_time;
+        int turnaround_time = completion_time - procs_copy[ip].arrival_time;
+        int waiting_time = turnaround_time - procs_copy[ip].burst_time;
+        total_wait += waiting_time;
+        total_turnaround += turnaround_time;
+      }
+    }
   }
   printf("Average Turnaround Time: %.3f\n",
-         total_turnaround / (float)num_procs);
-  printf("Average Wait Time: %.3f\n", total_wait / (float)num_procs);
-  printf("Throughput: %.3f\n", num_procs / (float)total_time);
+         total_turnaround / static_cast<float>(nprocs));
+  printf("Average Wait Time: %.3f\n", total_wait / static_cast<float>(nprocs));
+  printf("Throughput: %.3f\n", nprocs / static_cast<float>(total_time));
 }
 
 int main(int argc, char **argv) {
